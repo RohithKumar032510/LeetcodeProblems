9cc72b5183fc7d787e3c86166501dbc356cc7e1f
RohithKumar032510 <122031570+RohithKumar032510@users.noreply.github.com>
2024-05-27 21:40:27 +0530

    RemoveNthNodeFromEndofList.java

---
+++ RemoveNthNodeFromEndofList.java
@@ -0,0 +1,23 @@
+class Solution{
+    public ListNode removeNthFromEnd(ListNode head,int n){
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=0;i<n;i++){
+            if(fast.next==null){
+                if(i==n-1){
+                    head=head.next;
+                }
+                return head;
+            }
+          fast=fast.next;
+        }
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        if(slow.next!=null){
+            slow.next=slow.next.next;
+        }
+        return head;
+    }
+}
